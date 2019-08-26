dc9854ae22e483efef51787b7cd62bfcf6095471
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 18:40:11 +0700

    add generator

--- blocks/blocks_tilt-switch.js
+++ blocks/blocks_tilt-switch.js
@@ -1,15 +1,18 @@
-Blockly.Blocks['tilt_switch_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("TILT SWITCH");
-    this.appendValueInput("TILT_SWITCH_PIN")
-        .setCheck("Number")
-        .appendField("SWITCH PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["tilt_switch_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("TILT SWITCH");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "TILT_SWITCH_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("TILT_SWITCH_VALUE"),
+          "TILT_SWITCH_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_tilt-switch.js
+++ blocks/generators_tilt-switch.js
@@ -1,6 +1,20 @@
-Blockly.JavaScript['tilt_switch_block'] = function(block) {
-  var value_tilt_switch_pin = Blockly.JavaScript.valueToCode(block, 'TILT_SWITCH_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["tilt_switch_block"] = function(block) {
+    var text_tilt_switch_pin = block.getFieldValue("TILT_SWITCH_PIN");
+    var variable_tilt_switch_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("TILT_SWITCH_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+	int TILT_SWITCH_PIN = ${text_tilt_switch_pin}; // Definition of mercury tilt switch sensor interface
+	#END
+
+	#SETUP
+	pinMode(TILT_SWITCH_PIN, INPUT);
+	#END
+
+	${variable_tilt_switch_value} = digitalRead(TILT_SWITCH_PIN); // check mercury switch state
+    `;
+    return code;
+  };
